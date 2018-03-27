e1e200823be3a6b94473f8c7b6875c6851746852
Alexandra <a.ermilova@evotor.ru>
2018-03-27 09:46:51 +0300

    Create actchargeonshop entities, provide api

---
+++ api/src/main/java/ru/evotor/egais/api/model/document/actchargeonshop/ActChargeOnShop.kt
@@ -0,0 +1,33 @@
+package ru.evotor.egais.api.model.document.actchargeonshop
+
+import ru.evotor.egais.api.model.document.EgaisDocument
+import java.util.*
+
+/**
+ * Акт постановки на баланс в торговый зал
+ *
+ * @property uuid Уникальный идентификатор
+ * @property docOwner Кто подает документы
+ * @property identity ID акта (клиентсикй)
+ * @property number Номер документа
+ * @property actDate Дата постановски на баланс
+ * @property type Причина постановки на баланс
+ * @property actWriteOff Регистрационный номер акта списания по типу "Пересортица".
+ * Обязательно для заполнения для типа постановки на баланс "Пересортица".
+ * @property note Примечание
+ * @property status Статус документа
+ * @property rejectComment Комментарий при отказе
+ */
+data class ActChargeOnShop(
+        @JvmField val uuid: UUID,
+        @JvmField val docOwner: String,
+        @JvmField val identity: String?,
+        @JvmField val number: String,
+        @JvmField val actDate: Date?,
+        @JvmField val type: Type,
+        @JvmField val actWriteOff: String?,
+        @JvmField val note: String?,
+        @JvmField val status: Status,
+        @JvmField val rejectComment: String?
+) : EgaisDocument() {
+}
---
+++ api/src/main/java/ru/evotor/egais/api/model/document/actchargeonshop/ActChargeOnShopPosition.kt
@@ -0,0 +1,21 @@
+package ru.evotor.egais.api.model.document.actchargeonshop
+
+import java.math.BigDecimal
+import java.util.*
+
+/**
+ * Позиция в акте постановки на баланс
+ *
+ * @property uuid Уникальный идентификатор позиции акта
+ * @property actChargeOnShopUuid Идентификатор акта постановки, содержащего позицию
+ * @property identity Идентификатор позиции внутри накладной
+ * @property quantity Количество
+ * @property productInfoAlcCode Алкокод информации о продукции
+ */
+data class ActChargeOnShopPosition(
+        @JvmField val uuid: UUID,
+        @JvmField var actChargeOnShopUuid: UUID? = null,
+        @JvmField val identity: String?,
+        @JvmField val quantity: BigDecimal,
+        @JvmField val productInfoAlcCode: String?
+)
