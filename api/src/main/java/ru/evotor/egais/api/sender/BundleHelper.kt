4fcc5b547a732c1121cf610a8f77dafe6e1eaf3e
Alexandra <a.ermilova@evotor.ru>
2018-05-08 10:11:54 +0300

    Add javadoc (description) for bundle helper

--- api/src/main/java/ru/evotor/egais/api/sender/BundleHelper.kt
+++ api/src/main/java/ru/evotor/egais/api/sender/BundleHelper.kt
@@ -6,6 +6,9 @@ import ru.evotor.egais.api.model.document.ticket.DocType
 import ru.evotor.egais.api.model.document.waybillact.AcceptType
 import java.math.BigDecimal
 
+/**
+ * Класс для формирования Bundle из документов ЕГАИС и обратно
+ */
 object BundleHelper {
     private const val DOC_TYPE = "docType"
     private const val IDENTITY = "identity"
@@ -20,6 +23,17 @@ object BundleHelper {
     private const val REAL_QUANTITY = "realQuantity"
     private const val MARKS = "marks"
 
+    /**
+     * Добавление акта разногласий к ТТН в Bundle
+     *
+     * @param identity ИД Акта (клиентский)
+     * @param acceptType Тип подтверждения: Принимаем/отказываем
+     * @param number Номер акта
+     * @param wbRegId ИД накладной в системе
+     * @param version Версия протокола докумкетооборота ЕГАИС
+     * @param note Заметки
+     * @param positions Позиции акта разногласий к ТТН, сформированные в Bundle
+     */
     fun wayBillActToBundle(identity: String?, acceptType: AcceptType?, number: String, wbRegId: String, version: Version, note: String?, positions: List<Bundle>): Bundle {
         val bundle = Bundle()
         bundle.putString(DOC_TYPE, DocType.WAY_BILL_ACT.toString())
@@ -34,35 +48,76 @@ object BundleHelper {
         return bundle
     }
 
-    fun wayBillActPositionToBundle(identity: String, inforF2RegId: String, realQuantity: BigDecimal, marks: List<String>): Bundle {
+    /**
+     * Добавление позиции акта разногласий к ТТН в Bundle
+     *
+     * @param identity Идентификатор позиции внутри накладной
+     * @param informBRegId Регистрационный номер записи справки Б
+     * @param realQuantity Количество фактическое
+     * @param marks Перечень считанных кодов марок
+     */
+    fun wayBillActPositionToBundle(identity: String, informF2RegId: String, realQuantity: BigDecimal, marks: List<String>): Bundle {
         val bundle = Bundle()
         bundle.putString(IDENTITY, identity)
-        bundle.putString(INFORM_F2_REG_ID, inforF2RegId)
+        bundle.putString(INFORM_F2_REG_ID, informF2RegId)
         bundle.putSerializable(REAL_QUANTITY, realQuantity)
         bundle.putStringArrayList(MARKS, ArrayList(marks))
 
         return bundle
     }
 
+    /**
+     * Тип документа
+     */
     fun Bundle.getDocType() = getString(DOC_TYPE)
 
+    /**
+     * Идентификатор (клиентский)
+     */
     fun Bundle.getIdentity() = getString(IDENTITY)
 
+    /**
+     * Номер документа
+     */
     fun Bundle.getNumber() = getString(NUMBER)
 
+    /**
+     * ИД накладной в системе
+     */
     fun Bundle.getWbRegId() = getString(WB_REG_ID)
 
+    /**
+     * Заметки
+     */
     fun Bundle.getNote() = getString(NOTE)
 
+    /**
+     * Версия протокола докумкетооборота ЕГАИС
+     */
     fun Bundle.getVersion() = getString(VERSION)?.let { Version.valueOf(it) }
 
+    /**
+     * Регистрационный номер записи справки 2
+     */
     fun Bundle.getInformF2RegId() = getString(INFORM_F2_REG_ID)
 
+    /**
+     * Перечень считанных кодов марок
+     */
     fun Bundle.getMarks() = getStringArrayList(MARKS)
 
+    /**
+     * Список позиций документа, сформированных в Bundle
+     */
     fun Bundle.getPositions(): List<Bundle>? = getParcelableArrayList(POSITIONS)
 
+    /**
+     * Тип подтверждения: Принимаем/отказываем
+     */
     fun Bundle.getAcceptType() = getString(ACCEPT_TYPE)?.let { AcceptType.valueOf(it) }
 
+    /**
+     * Количество фактическое
+     */
     fun Bundle.getRealQuantity() = getSerializable(REAL_QUANTITY) as? BigDecimal
 }
