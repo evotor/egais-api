708d56ca959ddf017d020e474d2a3d4345fa2409
d.fabrichnyi <d.fabrichnyi@evotor.ru>
2018-05-29 11:21:52 +0300

    Add javadoc to EgaisDocumentStatusRecord

--- api/src/main/java/ru/evotor/egais/api/model/document/EgaisDocumentStatusRecord.kt
+++ api/src/main/java/ru/evotor/egais/api/model/document/EgaisDocumentStatusRecord.kt
@@ -4,9 +4,29 @@ import ru.evotor.egais.api.model.document.ticket.DocType
 import java.util.*
 
 data class EgaisDocumentStatusRecord(
+
+        /**
+         * UUID документа
+         * */
         val uuid: UUID,
+
+        /**
+         * Старый статус документа
+         * */
         val stateOld: String?,
+
+        /**
+         * Новый статус документа
+         * */
         val stateNew: String?,
+
+        /**
+         * Тип документа
+         * */
         val type: DocType,
+
+        /**
+         * Дата изменения статуса
+         * */
         val date: Date
 )
