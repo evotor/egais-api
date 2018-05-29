c5ea1bc00050f8eed8f20082da4cc630ec7b0bde
d.fabrichnyi <d.fabrichnyi@evotor.ru>
2018-05-29 15:22:17 +0300

    Remove status conversion methods

--- api/src/main/java/ru/evotor/egais/api/model/document/waybill/TtnInformF2RegStatus.kt
+++ api/src/main/java/ru/evotor/egais/api/model/document/waybill/TtnInformF2RegStatus.kt
@@ -2,14 +2,5 @@ package ru.evotor.egais.api.model.document.waybill
 
 enum class TtnInformF2RegStatus {
     NOT_PROCESSED,
-    FINISH;
-
-    companion object {
-        fun toString(status: TtnInformF2RegStatus): String {
-            return when (status) {
-                NOT_PROCESSED -> "Не обработано"
-                FINISH -> "Завершена"
-            }
-        }
-    }
+    FINISH
 }
--- api/src/main/java/ru/evotor/egais/api/model/document/waybillact/Status.kt
+++ api/src/main/java/ru/evotor/egais/api/model/document/waybillact/Status.kt
@@ -6,15 +6,5 @@ package ru.evotor.egais.api.model.document.waybillact
 enum class Status {
     NOT_PROCESSED,
     FINISH,
-    REJECTED;
-
-    companion object {
-        fun toString(status: Status): String {
-            return when (status) {
-                NOT_PROCESSED -> "Не обработано"
-                FINISH -> "Завершена"
-                REJECTED -> "Отказ"
-            }
-        }
-    }
+    REJECTED
 }
