b2a0e1420205c0b94e917ef33564dee0272d0293
Alexandra <a.ermilova@evotor.ru>
2018-10-05 11:09:35 +0300

    Implement alcohol control settings query

--- api/src/main/java/ru/evotor/egais/api/provider/client_settings/ClientSettingsContract.kt
+++ api/src/main/java/ru/evotor/egais/api/provider/client_settings/ClientSettingsContract.kt
@@ -7,8 +7,14 @@ internal object ClientSettingsContract {
     const val AUTHORITY = "${MainContract.AUTHORITY_PREFIX}.clientsettings"
 
     const val FSRAR_ID_COLUMN_NAME = "FSRAR_ID"
+    const val ALCOHOL_CONTROL_TYPE_COLUMN_NAME = "ALCOHOL_CONTROL_TYPE"
+    const val ALCOHOL_ENABLED_COLUMN_NAME = "ALCOHOL_ENABLED"
     const val FSRAR_ID_PATH = "FsRarId"
+    const val ALCOHOL_ENABLED_PATH = "AlcoholEnabled"
+    const val ALCOHOL_CONTROL_TYPE_PATH = "AlcoholControlType"
 
     val BASE_URI = Uri.parse("content://$AUTHORITY")
     val FSRAR_ID_URI = Uri.withAppendedPath(BASE_URI, FSRAR_ID_PATH)
+    val ALCOHOL_CONTROL_TYPE_URI = Uri.withAppendedPath(BASE_URI, ALCOHOL_CONTROL_TYPE_PATH)
+    val ALCOHOL_ENABLED_URI = Uri.withAppendedPath(BASE_URI, ALCOHOL_ENABLED_PATH)
 }
--- api/src/main/java/ru/evotor/egais/api/query/ClientSettingsQuery.kt
+++ api/src/main/java/ru/evotor/egais/api/query/ClientSettingsQuery.kt
@@ -1,6 +1,7 @@
 package ru.evotor.egais.api.query
 
 import android.content.Context
+import ru.evotor.egais.api.model.AlcoholControlType
 import ru.evotor.egais.api.provider.client_settings.ClientSettingsContract
 
 class ClientSettingsQuery {
@@ -11,5 +12,25 @@ class ClientSettingsQuery {
                     cursor.moveToFirst()
                     cursor.getString(cursor.getColumnIndex(ClientSettingsContract.FSRAR_ID_COLUMN_NAME))
                 }
+    }
+
+    fun getAlcoholControlType(context: Context): AlcoholControlType? {
+        return context.contentResolver.query(ClientSettingsContract.ALCOHOL_CONTROL_TYPE_URI, null, null, null, null)
+                ?.let { cursor ->
+                    cursor.moveToFirst()
+                    cursor.getString(cursor.getColumnIndex(ClientSettingsContract.ALCOHOL_CONTROL_TYPE_COLUMN_NAME))?.let {
+                        AlcoholControlType.valueOf(it)
+                    }
+                }
+    }
+
+    fun isAlcoholEnabled(context: Context): Boolean {
+        return context.contentResolver.query(ClientSettingsContract.ALCOHOL_ENABLED_URI, null, null, null, null)
+                ?.let { cursor ->
+                    cursor.moveToFirst()
+                    cursor.getString(cursor.getColumnIndex(ClientSettingsContract.ALCOHOL_ENABLED_COLUMN_NAME))?.let {
+                        it.toBoolean()
+                    }
+                } ?: true
     }
 }
