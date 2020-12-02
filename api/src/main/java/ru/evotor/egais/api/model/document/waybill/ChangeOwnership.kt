69010c3c27a57d69b27260bce8ccaaf8b6f68391
Alexandra <a.ermilova@evotor.ru>
2020-12-02 12:00:05 +0300

    STDEV-6552: Document change ownership enum values

--- api/src/main/java/ru/evotor/egais/api/model/document/waybill/ChangeOwnership.kt
+++ api/src/main/java/ru/evotor/egais/api/model/document/waybill/ChangeOwnership.kt
@@ -5,12 +5,12 @@ package ru.evotor.egais.api.model.document.waybill
  */
 enum class ChangeOwnership {
     /**
-     *
+     * Изменение права собственности
      */
     IS_CHANGE,
 
     /**
-     *
+     * Без изменения права собственности
      */
     NOT_CHANGE
 }
