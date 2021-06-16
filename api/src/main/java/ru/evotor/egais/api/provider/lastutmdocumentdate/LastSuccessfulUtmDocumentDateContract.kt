df079482615253c2290abf62093fe90a224871df
Dmitry Sementsov <d.sementsov@evotor.ru>
2021-06-16 22:28:28 +0300

    Added getting the date of the last document in UTM.

---
+++ api/src/main/java/ru/evotor/egais/api/provider/lastutmdocumentdate/LastSuccessfulUtmDocumentDateContract.kt
@@ -0,0 +1,12 @@
+package ru.evotor.egais.api.provider.lastutmdocumentdate
+
+import android.net.Uri
+import ru.evotor.egais.api.provider.MainContract
+
+internal object LastSuccessfulUtmDocumentDateContract {
+    const val AUTHORITY = "${MainContract.AUTHORITY_PREFIX}.lastutmdocumentdate"
+    const val CREATION_DATE_COLUMN_NAME = "CREATION_DATE"
+    val LAST_UTM_DOCUMENT_PATH = "LastUtmDocumentDate"
+    val BASE_URI = Uri.parse("content://$AUTHORITY")
+    val LAST_UTM_DOCUMENT_URI = Uri.withAppendedPath(BASE_URI, LAST_UTM_DOCUMENT_PATH)
+}
