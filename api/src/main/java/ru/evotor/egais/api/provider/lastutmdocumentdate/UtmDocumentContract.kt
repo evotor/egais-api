91b9be943b4e648b49bd76b8f844b97a153589a4
Dmitry Sementsov <d.sementsov@evotor.ru>
2021-06-29 17:15:34 +0300

    Added getting the date of the last query rests documents in UTM. df07948

--- api/src/main/java/ru/evotor/egais/api/provider/lastutmdocumentdate/UtmDocumentContract.kt
+++ api/src/main/java/ru/evotor/egais/api/provider/lastutmdocumentdate/UtmDocumentContract.kt
@@ -5,9 +5,16 @@ import ru.evotor.egais.api.provider.MainContract
 
 internal object UtmDocumentContract {
     const val AUTHORITY = "${MainContract.AUTHORITY_PREFIX}.utmdocument"
-    const val CREATION_DATE_COLUMN_NAME = "CREATION_DATE"
-    val UTM_DOCUMENT_PATH = "UtmDocument"
     val BASE_URI = Uri.parse("content://$AUTHORITY")
+
+    const val UTM_DOCUMENT_PATH = "UtmDocument"
+    const val QUERY_RESTS_PATH = "QueryRests"
+    const val QUERY_RESTS_SHOP_PATH = "QueryRestsShop"
+
     val UTM_DOCUMENT_URI = Uri.withAppendedPath(BASE_URI, UTM_DOCUMENT_PATH)
+    val QUERY_RESTS_URI = Uri.withAppendedPath(BASE_URI, QUERY_RESTS_PATH)
+    val QUERY_RESTS_SHOP_URI = Uri.withAppendedPath(BASE_URI, QUERY_RESTS_SHOP_PATH)
+
+    const val CREATION_DATE_COLUMN_NAME = "CREATION_DATE"
     const val DATE_FORMAT_CREATION_DATE = "yyyy-MM-dd HH:mm:ss"
 }
--- api/src/main/java/ru/evotor/egais/api/query/UtmDocumentQuery.kt
+++ api/src/main/java/ru/evotor/egais/api/query/UtmDocumentQuery.kt
@@ -11,6 +11,24 @@ class UtmDocumentQuery {
                 ?.use { cursor ->
                     cursor.moveToFirst()
                     cursor.getString(cursor.getColumnIndex(UtmDocumentContract.CREATION_DATE_COLUMN_NAME))
+                             .let { SimpleDateFormat(UtmDocumentContract.DATE_FORMAT_CREATION_DATE, Locale.getDefault()).parse(it) }
+                }
+    }
+
+    fun getLastQueryRestsUtmDocumentDate(context: Context): Date? {
+        return context.contentResolver.query(UtmDocumentContract.QUERY_RESTS_URI, null, null, null, null)
+                ?.use { cursor ->
+                    cursor.moveToFirst()
+                    cursor.getString(cursor.getColumnIndex(UtmDocumentContract.CREATION_DATE_COLUMN_NAME))
+                            .let { SimpleDateFormat(UtmDocumentContract.DATE_FORMAT_CREATION_DATE, Locale.getDefault()).parse(it) }
+                }
+    }
+
+    fun getLastQueryRestsShopUtmDocumentDate(context: Context): Date? {
+        return context.contentResolver.query(UtmDocumentContract.QUERY_RESTS_SHOP_URI, null, null, null, null)
+                ?.use { cursor ->
+                    cursor.moveToFirst()
+                    cursor.getString(cursor.getColumnIndex(UtmDocumentContract.CREATION_DATE_COLUMN_NAME))
                             .let { SimpleDateFormat(UtmDocumentContract.DATE_FORMAT_CREATION_DATE, Locale.getDefault()).parse(it) }
                 }
     }
