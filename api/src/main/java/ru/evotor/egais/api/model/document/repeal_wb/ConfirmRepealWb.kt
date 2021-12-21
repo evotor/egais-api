fbd52fd4d1198db3d67574a4a1f2065d0e75dc7b
Dmitry Sementsov <d.sementsov@evotor.ru>
2021-12-21 17:35:06 +0300

    STDEV-10034. Changed REQUEST_REPEAL_WB and CONFIRM_REPEAL_WB.

--- api/src/main/java/ru/evotor/egais/api/model/document/repeal_wb/ConfirmRepealWb.kt
+++ api/src/main/java/ru/evotor/egais/api/model/document/repeal_wb/ConfirmRepealWb.kt
@@ -1,22 +1,34 @@
 package ru.evotor.egais.api.model.document.repeal_wb
 
 import ru.evotor.egais.api.model.document.EgaisDocument
+import ru.evotor.egais.api.model.document.ticket.ConfirmTicketStatus
 import java.util.*
 
 /**
  * Ответ на запрос об отмене проведения акта для ТТН
+ *
+ * @property uuid Уникальный идентификатор
+ * @property docOwner Кто подает документы
  * @property identity Идентификатор Акта (клиентский)
- * @property confirmType Тип подтверждения: Принимаем/отказываем
+ * @property isConfirm Тип подтверждения: Принимаем/отказываем
  * @property number Номер документа
  * @property date Дата документа
  * @property wbRegId Идентификатор накладной в системе
  * @property note Заметки
+ * @property status Статус документа
+ * @property rejectComment Комментарий при отказе
+ * @property replyId Уникальный идентификатор документа (присваивается УТМ); совпадает с идентификатором исходящего документа, который получили в ответе
  */
 data class ConfirmRepealWb(
+    val uuid: UUID,
+    val docOwner: String,
     val identity: String?,
-    val confirmType: ConfirmType,
-    val number: String,
+    val isConfirm: ConfirmType?,
+    val number: String?,
     val date: Date?,
-    val wbRegId: String,
-    val note: String?
+    val wbRegId: String?,
+    val note: String?,
+    val status: ConfirmTicketStatus,
+    val rejectComment: String?,
+    val replyId: String?
 ) : EgaisDocument()
--- api/src/main/java/ru/evotor/egais/api/model/document/repeal_wb/RequestRepealWb.kt
+++ api/src/main/java/ru/evotor/egais/api/model/document/repeal_wb/RequestRepealWb.kt
@@ -1,20 +1,32 @@
 package ru.evotor.egais.api.model.document.repeal_wb
 
 import ru.evotor.egais.api.model.document.EgaisDocument
+import ru.evotor.egais.api.model.document.ticket.ConfirmTicketStatus
 import java.util.*
 
 /**
  * Запрос на отмену проведения акта для ТТН
- * @param identity ИД Акта (клиентский)
- * @param number Номер документа
- * @param date Дата создания запроса
- * @param wbRegId ID ТТН в системе
- * @param note Примечание
+ *
+ * @property uuid Уникальный идентификатор
+ * @property docOwner Кто подает документы
+ * @property identity ИД Акта (клиентский)
+ * @property number Номер документа
+ * @property date Дата создания запроса
+ * @property wbRegId ID ТТН в системе
+ * @property note Примечание
+ * @property status Статус документа
+ * @property rejectComment Комментарий при отказе
+ * @property replyId Уникальный идентификатор документа (присваивается УТМ); совпадает с идентификатором исходящего документа, который получили в ответе
  */
 data class RequestRepealWb(
+    val uuid: UUID,
+    val docOwner: String,
     val identity: String?,
     val number: String,
     val date: Date?,
     val wbRegId: String,
-    val note: String?
+    val note: String?,
+    val status: ConfirmTicketStatus,
+    val rejectComment: String?,
+    val replyId: String?
 ) : EgaisDocument()
