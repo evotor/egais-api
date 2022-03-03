d6cd51e0a75ddcf8df1a113b87e3de0c46c1cf9c
Dmitry Sementsov <d.sementsov@evotor.ru>
2022-03-03 12:18:22 +0300

    STDEV-10509. WayBill and WayBillAct statuses are made the same.

--- api/src/main/java/ru/evotor/egais/api/model/document/waybill/Status.kt
+++ api/src/main/java/ru/evotor/egais/api/model/document/waybill/Status.kt
@@ -1,15 +1,59 @@
 package ru.evotor.egais.api.model.document.waybill
 
+/**
+ * Статус ТТН
+ */
 enum class Status {
+    /**
+     * Новая
+     */
     NOT_PROCESSED,
+    /**
+     * Ожидание данных от УТМ
+     */
     WAIT_UTM_ANSWER,
+    /**
+     * Принята
+     */
     FINISH,
+    /**
+     * Отклонена
+     */
     NOT_CONFIRMED,
+    /**
+     * Ожидание ответа от отправителя
+     */
     WAIT_SENDER_ANSWER,
+    /**
+     * Акт разногласий отклонен
+     */
     REJECTED_WBA,
+    /**
+     * Распроведена (Отмена проведения акта накладной)
+     */
     REPEALED,
+    /**
+     * Ошибка
+     */
     ERROR,
+    /**
+     * Нулевой акт расхождения
+     */
     ZERO_QUANTITY_DIFFERENCES,
+    /**
+     * Новая версия
+     */
     NEW_VERSION,
-    CANCEL_WB
+    /**
+     * Отозвана
+     */
+    CANCEL_WB,
+    /**
+     * Запрошена отмена акта накладной (Отмена проведения акта накладной)
+     */
+    WAIT_REPEALED_ANSWER,
+    /**
+     * Отказ в распроведении (Отказ грузоотправителя от отмены проведения акта ТТН)
+     */
+    REJECTED_REPEAL
 }
--- api/src/main/java/ru/evotor/egais/api/model/document/waybillact/Status.kt
+++ api/src/main/java/ru/evotor/egais/api/model/document/waybillact/Status.kt
@@ -4,11 +4,56 @@ package ru.evotor.egais.api.model.document.waybillact
  * Статус акта разногласия для ТТН
  */
 enum class Status {
+    /**
+     * Новый
+     */
     NOT_PROCESSED,
+    /**
+     * Ожидание данных от УТМ
+     */
     WAIT_UTM_ANSWER,
-    WAIT_SENDER_ANSWER,
+    /**
+     * Принят
+     */
     FINISH,
-    REJECTED,
+    /**
+     * Отклонен (Полный отказ от приемки ТТН)
+     */
+    NOT_CONFIRMED,
+    /**
+     * Ожидание ответа от отправителя
+     */
+    WAIT_SENDER_ANSWER,
+    /**
+     * Акт разногласий отклонен
+     */
+    REJECTED_WBA,
+    /**
+     * Распроведен (Отмена проведения акта ТТН)
+     */
+    REPEALED,
+    /**
+     * Ошибка
+     */
     ERROR,
-    REPEALED
+    /**
+     * Нулевой акт расхождения
+     */
+    ZERO_QUANTITY_DIFFERENCES,
+    /**
+     * Новая версия
+     */
+    NEW_VERSION,
+    /**
+     * Отозвана
+     */
+    CANCEL_WB,
+    /**
+     * Запрошена отмена акта накладной (Отмена проведения акта ТТН)
+     */
+    WAIT_REPEALED_ANSWER,
+    /**
+     * Отказ в распроведении (Отказ грузоотправителя от отмены проведения акта ТТН)
+     */
+    REJECTED_REPEAL
 }
