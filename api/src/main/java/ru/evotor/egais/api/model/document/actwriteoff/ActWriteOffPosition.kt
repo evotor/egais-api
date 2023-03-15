4af25a7e3ac335bd04819542d60f82b03f2a1b1b
Dmitry Sementsov <d.sementsov@evotor.ru>
2023-03-15 19:56:19 +0300

    STDEV-14259. Added receipt UUID to ActWriteOffPosition.

--- api/src/main/java/ru/evotor/egais/api/model/document/actwriteoff/ActWriteOffPosition.kt
+++ api/src/main/java/ru/evotor/egais/api/model/document/actwriteoff/ActWriteOffPosition.kt
@@ -15,16 +15,18 @@ import java.util.*
  * @property informF2MarkInfoJson Информация о марках в формате JSON
  * @property productInfo Информация о продукции
  * @property sumSale Сумма продажи. Обязательно для заполнения при причине списания "Реализация"
+ * @property receiptUuid UUID чека продажи
  * @property markList Перечень считанных кодов марок
  */
 data class ActWriteOffPosition(
-        @JvmField val uuid: UUID,
-        @JvmField val actWriteOffUuid: UUID,
-        @JvmField val identity: String?,
-        @JvmField val quantity: BigDecimal,
-        @JvmField val informF2RegId: String?,
-        @JvmField val informF2MarkInfoJson: String?,
-        @JvmField val productInfo: ProductInfo,
-        @JvmField val sumSale: BigDecimal?,
-        @JvmField val markList: List<String>
+    @JvmField val uuid: UUID,
+    @JvmField val actWriteOffUuid: UUID,
+    @JvmField val identity: String?,
+    @JvmField val quantity: BigDecimal,
+    @JvmField val informF2RegId: String?,
+    @JvmField val informF2MarkInfoJson: String?,
+    @JvmField val productInfo: ProductInfo,
+    @JvmField val sumSale: BigDecimal?,
+    @JvmField val receiptUuid: String?,
+    @JvmField val markList: List<String>
 )
