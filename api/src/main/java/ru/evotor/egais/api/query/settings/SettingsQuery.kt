46dcf2d85b1af988d551cdb0fd8f97a9ad3ed176
Anna Balakireva <a.balakireva@evotor.ru>
2023-07-10 09:57:23 +0300

    STDEV-15696 Add beer taps settings query

--- api/src/main/java/ru/evotor/egais/api/query/settings/SettingsQuery.kt
+++ api/src/main/java/ru/evotor/egais/api/query/settings/SettingsQuery.kt
@@ -7,44 +7,94 @@ import ru.evotor.egais.api.provider.settings.SettingsContract
 class SettingsQuery {
 
     fun getAlcoholControlType(context: Context): AlcoholControlType? {
-        return context.contentResolver.query(SettingsContract.ALCOHOL_CONTROL_TYPE_URI, null, null, null, null)
-                ?.use { cursor ->
-                    cursor.moveToFirst()
-                    cursor.getString(cursor.getColumnIndex(SettingsContract.ALCOHOL_CONTROL_TYPE_COLUMN_NAME))?.let {
+        return context.contentResolver.query(
+            SettingsContract.ALCOHOL_CONTROL_TYPE_URI,
+            null,
+            null,
+            null,
+            null
+        )
+            ?.use { cursor ->
+                cursor.moveToFirst()
+                cursor.getString(cursor.getColumnIndex(SettingsContract.ALCOHOL_CONTROL_TYPE_COLUMN_NAME))
+                    ?.let {
                         AlcoholControlType.valueOf(it)
                     }
-                }
+            }
     }
 
     fun isAlcoholEnabled(context: Context): Boolean {
-        return context.contentResolver.query(SettingsContract.ALCOHOL_ENABLED_URI, null, null, null, null)
-                ?.use { cursor ->
-                    cursor.moveToFirst()
-                    cursor.getString(cursor.getColumnIndex(SettingsContract.ALCOHOL_ENABLED_COLUMN_NAME))?.toBoolean()
-                } ?: true
+        return context.contentResolver.query(
+            SettingsContract.ALCOHOL_ENABLED_URI,
+            null,
+            null,
+            null,
+            null
+        )
+            ?.use { cursor ->
+                cursor.moveToFirst()
+                cursor.getString(cursor.getColumnIndex(SettingsContract.ALCOHOL_ENABLED_COLUMN_NAME))
+                    ?.toBoolean()
+            } ?: true
     }
 
     fun isLinkEgaisCommodity(context: Context): Boolean {
-        return context.contentResolver.query(SettingsContract.LINK_EGAIS_COMMODITY_URI, null, null, null, null)
-                ?.use { cursor ->
-                    cursor.moveToFirst()
-                    cursor.getString(cursor.getColumnIndex(SettingsContract.LINK_EGAIS_COMMODITY_COLUMN_NAME))?.toBoolean()
-                } ?: true
+        return context.contentResolver.query(
+            SettingsContract.LINK_EGAIS_COMMODITY_URI,
+            null,
+            null,
+            null,
+            null
+        )
+            ?.use { cursor ->
+                cursor.moveToFirst()
+                cursor.getString(cursor.getColumnIndex(SettingsContract.LINK_EGAIS_COMMODITY_COLUMN_NAME))
+                    ?.toBoolean()
+            } ?: true
     }
 
     fun isUtmSyncEnabled(context: Context): Boolean {
-        return context.contentResolver.query(SettingsContract.UTM_SYNCHRONIZATION_URI, null, null, null, null)
-                ?.use { cursor ->
-                    cursor.moveToFirst()
-                    cursor.getString(cursor.getColumnIndex(SettingsContract.UTM_SYNCHRONIZATION_COLUMN_NAME))?.toBoolean()
-                } ?: true
+        return context.contentResolver.query(
+            SettingsContract.UTM_SYNCHRONIZATION_URI,
+            null,
+            null,
+            null,
+            null
+        )
+            ?.use { cursor ->
+                cursor.moveToFirst()
+                cursor.getString(cursor.getColumnIndex(SettingsContract.UTM_SYNCHRONIZATION_COLUMN_NAME))
+                    ?.toBoolean()
+            } ?: true
     }
 
     fun isCloudSyncEnabled(context: Context): Boolean {
-        return context.contentResolver.query(SettingsContract.CLOUD_SYNCHRONIZATION_URI, null, null, null, null)
-                ?.use { cursor ->
-                    cursor.moveToFirst()
-                    cursor.getString(cursor.getColumnIndex(SettingsContract.CLOUD_SYNCHRONIZATION_COLUMN_NAME))?.toBoolean()
-                } ?: true
+        return context.contentResolver.query(
+            SettingsContract.CLOUD_SYNCHRONIZATION_URI,
+            null,
+            null,
+            null,
+            null
+        )
+            ?.use { cursor ->
+                cursor.moveToFirst()
+                cursor.getString(cursor.getColumnIndex(SettingsContract.CLOUD_SYNCHRONIZATION_COLUMN_NAME))
+                    ?.toBoolean()
+            } ?: true
+    }
+
+    fun isBeerTapsEnabled(context: Context): Boolean {
+        return context.contentResolver.query(
+            SettingsContract.BEER_TAPS_ENABLED_URI,
+            null,
+            null,
+            null,
+            null
+        )
+            ?.use { cursor ->
+                cursor.moveToFirst()
+                cursor.getString(cursor.getColumnIndex(SettingsContract.BEER_TAPS_ENABLED_COLUMN_NAME))
+                    ?.toBoolean()
+            } ?: true
     }
 }
